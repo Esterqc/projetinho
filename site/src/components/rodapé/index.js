209d50776e46929c39aa4521a682db4354800680
maria clara <mabentarosa@gmail.com>
2023-01-06 16:15:34 -0300

    subir home

---
+++ site/src/components/rodapé/index.js
@@ -0,0 +1,19 @@
+
+import './index.scss'
+
+import Logo from '../../assets/logo.png'
+
+export default function Rodape() {
+    return(
+        
+      <div className='Principal-Rodape'>
+
+           <div className='Primeiro-Container-Rodape'>
+           
+           </div>
+       
+      </div>
+
+      
+    )
+}
--- site/src/pages/home/index.js
+++ site/src/pages/home/index.js
@@ -2,9 +2,19 @@
 import './index.scss'
 
 import { useNavigate, Link } from 'react-router-dom';
+
+import Comentarios from '../../assets/comentarios.png'
+import Rodapé from '../../components/rodapé/index.js'
+
 import Img1 from '../../assets/img1.png'
 import Img2 from '../../assets/img2.png'
 import Img3 from '../../assets/img3.png'
+import Img4 from '../../assets/img4.png'
+import Img5 from '../../assets/img5.png'
+import Img6 from '../../assets/img6.png'
+import Img7 from '../../assets/img7.png'
+import Img8 from '../../assets/img8.png'
+
 
 
 import Cabecalho from '../../components/faixa'
@@ -23,7 +33,8 @@ export default function Home() {
             <Link to='/servicosqRealizamos'>Serviços que Realizamos</Link>
             <Link to='/FotosEtrabalhos'>Fotos</Link>
             <Link to='/AgendarOrcamento'>Orçamento</Link>
-            <a>Entrar</a>
+            <Link to='/LoginCliente'>Entrar</Link>
+
           </div>
           <div className='Carrossel'></div>
           
@@ -43,7 +54,49 @@ export default function Home() {
           </div>
         </div>
 
-        
+        <div className='Container3'>
+
+         <p className='Paragrafo'>Nossos Trabalhos</p>
+
+
+         <div className='Imagens-Nossos-Trabalhos'>
+         <img src={Img4} alt='imagem4'/>
+          <img src={Img5} alt='imagem5'/>
+          <img src={Img6} alt='imagem6'/>
+          <img src={Img7} alt='imagem7'/>
+          <img src={Img8} alt='imagem8'/>
+         </div>
+
+        </div>
+
+        <div className='Container4'>
+
+      
+<p className='Paragrafo1'>Depoimentos dos Clientes
+</p> 
+    
+    
+<div className='Imagens-Depoimentos-dos-Clientes'>
+
+
+
+<img src={Comentarios} alt='Comentarios'/>
+<img src={Comentarios} alt='Comentarios'/>
+<img src={Comentarios} alt='Comentarios'/>
+<img src={Comentarios} alt='Comentarios'/>
+<img src={Comentarios} alt='Comentarios'/>
+
+</div>
+
+ <div className='Mas-Depoimentos'>
+ <button className='Mas-Depoimentos'>Mas Depoimentos...</button>
+ </div>
+
+
+</div>
+
+   <Rodapé/>
+
         </div>
 
         
