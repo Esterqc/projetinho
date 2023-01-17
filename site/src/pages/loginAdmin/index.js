af9e5bf75359a3a0348ad4f1d2b8093042979676
Maria Clara <mabentarosa@gmail.com>
2023-01-17 19:03:50 -0300

    Tela loginAdmin concluida

--- site/src/pages/loginAdmin/index.js
+++ site/src/pages/loginAdmin/index.js
@@ -1,11 +1,46 @@
 
 import './index.scss'
 
+import Axios from 'axios'
+import {useNavigate} from 'react-router-dom'
+
 import ImgLogin from '../../assets/imgLogin.png'
 import Cabecalho2 from '../../components/faixa2'
+import { useState } from 'react'
+import axios from 'axios'
 
 
 export default function LoginAdmin() {
+ 
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const [erro, setErro] = useState('');
+
+
+    const navigate = useNavigate()
+
+    async function EntrarClick() {
+try{
+
+      
+    const Login = await axios.post('http://localhost:5000/api/login', {
+        email: email,
+        senha: senha
+    });
+   
+        navigate('/TelaAdministrador');
+    
+
+  
+} catch (err) {
+  if(err.response.status === 401){
+    setErro(err.response.data.erro);
+  }
+ 
+}
+
+}
+
     return(
         
         <div className='Page-Login'>
@@ -13,15 +48,29 @@ export default function LoginAdmin() {
 
                <div className='Container-Imagem'>
                   <img src={ImgLogin} alt='ImgLogin'/>
-                 
-              
+             </div>
 
-               <div className='Container-Imagem2'>
-               
+             <div className="Container-Login">
 
-                  </div>
+                <p className='Paragrafo-Login'><h2>LOGIN</h2></p>
 
-                  </div>
+                <p className='Paragrafo-Email'>EMAIL :</p>
+                <input className='Input-Email' value={email} onChange={e => setEmail(e.target.value)}></input>
+
+                <p className='Paragrafo-Senha'>SENHA :</p>
+                <input type="password" className='Input-Senha'  value={senha} onChange={e => setSenha(e.target.value)}></input>
+                
+                <button className='Entrar' onClick={EntrarClick}> ENTRAR</button>
+
+                <hr className='Linha'/>
+
+                <button className='Cancelar'><p>Cancelar</p></button>
+
+                <div className='Erro'>
+                    {erro}
+                    </div>
+             </div>
+
 
                 
                  
